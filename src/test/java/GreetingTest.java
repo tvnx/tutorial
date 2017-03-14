c677ff4effd609320d6137b1a9dd394926d6d852
Korbinian <korbinian@remeeting.com>
2017-03-15 00:43:37 +0100

    Updated to JUnit5

--- src/test/java/GreetingTest.java
+++ src/test/java/GreetingTest.java
@@ -1,9 +1,9 @@
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
 
-import static org.junit.Assert.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class GreetingTest {
 	@Test
